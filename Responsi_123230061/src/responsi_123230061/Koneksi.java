38d468b9f18baca6acf1ac9732679237e19f72bf
pradipaaaa <150516554+pradipaaaa@users.noreply.github.com>
2025-05-28 12:28:02 +0700

    Responsi

---
+++ Responsi_123230061/src/responsi_123230061/Koneksi.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package responsi_123230061;
+
+/**
+ *
+ * @author Lab Informatika
+ */
+public class Koneksi {
+    
+}
